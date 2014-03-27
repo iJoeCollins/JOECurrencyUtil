6b5f4e99be6e0f05b43e555903576e3a110d1a93
Joseph Collins <joe@ijoe.co>
2014-03-27 18:06:06 -0400

    Initial Commit

---
+++ JOECurrencyUtil/JOEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JOEAppDelegate.h
+//  JOECurrencyUtil
+//
+//  Created by Joseph Collins on 3/27/14.
+//  Copyright (c) 2014 Joseph Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JOECurrencyUtil/JOEViewController.h
@@ -0,0 +1,13 @@
+//
+//  JOEViewController.h
+//  JOECurrencyUtil
+//
+//  Created by Joseph Collins on 3/27/14.
+//  Copyright (c) 2014 Joseph Collins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JOEViewController : UIViewController
+
+@end
